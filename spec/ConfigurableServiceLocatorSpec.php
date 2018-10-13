d60ff5bdf4f7b342e01a9384b23b3490d226a0ae
Dan Murfitt <dan@murfitt.net>
2018-10-13 15:49:42 +0100

    It can return an instance of a class from configuration.

--- spec/ConfigurableServiceLocatorSpec.php
+++ spec/ConfigurableServiceLocatorSpec.php
@@ -5,9 +5,15 @@ namespace spec\danmurf\DependencyInjection;
 use danmurf\DependencyInjection\ConfigurableServiceLocator;
 use danmurf\DependencyInjection\ServiceLocatorInterface;
 use PhpSpec\ObjectBehavior;
+use Psr\Container\ContainerInterface;
 
 class ConfigurableServiceLocatorSpec extends ObjectBehavior
 {
+    public function let(ContainerInterface $container)
+    {
+        $this->beConstructedWith([]);
+    }
+
     public function it_is_initializable()
     {
         $this->shouldHaveType(ConfigurableServiceLocator::class);
@@ -16,5 +22,25 @@ class ConfigurableServiceLocatorSpec extends ObjectBehavior
     public function it_implements_service_locator_interface()
     {
         $this->shouldImplement(ServiceLocatorInterface::class);
+    }
+
+    public function it_can_return_a_new_instance_of_a_service_from_configuration(ContainerInterface $container)
+    {
+        $config = [
+            'my.service.id' => [
+                'class' => ConfigurableServiceLocatorTestClass::class,
+            ],
+        ];
+
+        $this->beConstructedWith($config);
+
+        $this->locate('my.service.id', $container)->shouldReturnAnInstanceOf(ConfigurableServiceLocatorTestClass::class);
+    }
+}
+
+class ConfigurableServiceLocatorTestClass
+{
+    public function __construct()
+    {
     }
 }
--- src/ConfigurableServiceLocator.php
+++ src/ConfigurableServiceLocator.php
@@ -2,11 +2,34 @@
 
 namespace danmurf\DependencyInjection;
 
+use danmurf\DependencyInjection\Exception\ContainerException;
+use danmurf\DependencyInjection\Exception\NotFoundException;
 use Psr\Container\ContainerInterface;
+use ReflectionClass;
 
 class ConfigurableServiceLocator implements ServiceLocatorInterface
 {
+    private $config;
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
     public function locate(string $id, ContainerInterface $container)
     {
+        if (!isset($this->config[$id])) {
+            throw new NotFoundException('Unable to locate service from configuration.');
+        }
+
+        if (!isset($this->config[$id]['class'])) {
+            throw new ContainerException(sptintf('Configured service `%s` has no `class` value.', $id));
+        }
+
+        $class = new ReflectionClass($this->config[$id]['class']);
+
+        $instance = $class->newInstance();
+
+        return $instance;
     }
 }
